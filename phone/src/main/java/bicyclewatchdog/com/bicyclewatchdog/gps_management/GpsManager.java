2921588a974c5f57abd44bbfed7b55474bb4acde
William <williamstogin2018@u.northwestern.edu>
2017-04-22 12:02:02 -0500

    Fixed implementation to use the LocationListener in documentation

--- phone/src/main/java/bicyclewatchdog/com/bicyclewatchdog/gps_management/GpsManager.java
+++ phone/src/main/java/bicyclewatchdog/com/bicyclewatchdog/gps_management/GpsManager.java
@@ -1,31 +1,22 @@
 package bicyclewatchdog.com.bicyclewatchdog.gps_management;
 
-import java.util.Timer;
+import android.location.LocationManager;
 
 /**
  * Created by William on 4/22/2017.
  */
 
 public class GpsManager {
-
-    private GpsPollingTask pollingTask;
-    private Timer mTimer;
+    LocationManager mLocationManager;
 
     /**
      * Initializes GpsManager to check coordinates every interval milliseconds
-     * @param interval in milliseconds between GPS polling
+     * @param threshold of distance changed (in meters) before update received
      */
-    public GpsManager(int interval) {
-        //TODO: initialize mTimer and pollingTask and set to run every interval ms
-        // See http://stackoverflow.com/questions/18353689/how-to-repeat-a-task-after-a-fixed-amount-of-time-in-android
-    }
-
-    /**
-     * Registers callback to run when gps coordinates change more than the threshold
-     * @param callback GpsCallback to be run
-     */
-    public void registerCallback(GpsCallback callback) {
-        //TODO: register the callback with the task if callback not already registered
+    public GpsManager(float threshold) {
+        //TODO: initialize mLocationManager
+        // Todo: set WatchdogListener to run every time threshold is met
+        // See https://developer.android.com/guide/topics/location/strategies.html
     }
 
 }
