45e2f9d9f17caaa13d79f2b272c13e73430e4c04
William <williamstogin2018@u.northwestern.edu>
2017-04-24 16:25:53 -0500

    Added message manager

---
+++ phone/src/main/java/bicyclewatchdog/com/bicyclewatchdog/message_management/MessageManager.java
@@ -0,0 +1,18 @@
+package bicyclewatchdog.com.bicyclewatchdog.message_management;
+
+/**
+ * Class used to send SMS messages
+ * Created by William on 4/24/2017.
+ */
+
+public class MessageManager {
+
+    /**
+     * Sends content to
+     * @param content
+     */
+    public void sendMessage(String content) {
+        //TODO: send the message
+    }
+
+}
