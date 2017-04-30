98d3a8af477a9c0635821a8c36a611f47f8a03c7
William <williamstogin2018@u.northwestern.edu>
2017-04-30 13:40:11 -0600

    Put together the bones for activity-service communication using the callback-binder pattern

--- phone/src/main/java/bicyclewatchdog/com/bicyclewatchdog/MainActivity.java
+++ phone/src/main/java/bicyclewatchdog/com/bicyclewatchdog/MainActivity.java
@@ -1,13 +1,21 @@
 package bicyclewatchdog.com.bicyclewatchdog;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WatchdogService.Callbacks{
     private static final String TAG = "MainActivity";
+
+    // Reference to the service
+    WatchdogService mService;
 
     //TODO: Add IPC to communicate with service
     //TODO: Add buttons to pair to bike
@@ -29,11 +37,36 @@ public class MainActivity extends AppCompatActivity {
         // Set the change listener for phone number
         this.findViewById(R.id.editTextPhone)
                 .setOnFocusChangeListener(new PhoneNumberChangedListener());
+
+        Intent serviceIntent = new Intent(this, WatchdogService.class);
+        bindService(serviceIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
+    }
+
 
     public void onPairClicked(View v) {
         Log.v(TAG, "Pair clicked.");
 
     }
 
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.v(TAG, "Connected!");
+            mService = ((WatchdogService.LocalBinder) service).getServiceInstance();
+            mService.registerCallbacks(MainActivity.this);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.v(TAG, "Service disconnected.");
+            mService = null;
+        }
+    };
 }
--- phone/src/main/java/bicyclewatchdog/com/bicyclewatchdog/WatchdogService.java
+++ phone/src/main/java/bicyclewatchdog/com/bicyclewatchdog/WatchdogService.java
@@ -2,19 +2,34 @@ package bicyclewatchdog.com.bicyclewatchdog;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
+import bicyclewatchdog.com.bicyclewatchdog.bluetooth_management.CustomBluetoothManager;
 import bicyclewatchdog.com.bicyclewatchdog.gps_management.GpsManager;
+import bicyclewatchdog.com.bicyclewatchdog.message_management.MessageManager;
 
 public class WatchdogService extends Service {
     private static final String TAG = "WatchdogService";
     private final float DEFAULT_THRESHOLD = 100; // Default to 100 meter change
+
+    // Activity reference for updating UI
+    Callbacks activity;
+    private final IBinder mBinder = new LocalBinder();
+
+    // Managers
+    private CustomBluetoothManager btManager;
     private GpsManager mGpsManager;
+    private MessageManager messageManager;
 
 
     public WatchdogService() {
-        // TODO: init mGpsManager with a threshold
+        // Init managers
+        btManager = new CustomBluetoothManager(getApplicationContext(), "asdf");
+        mGpsManager = new GpsManager(btManager);
+        messageManager = new MessageManager();
+
     }
 
     //TODO: Add IPC to receive new instructions from MainActivity
@@ -23,11 +38,31 @@ public class WatchdogService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        activity = null;
+        return false;
+    }
+
+
+    public interface Callbacks {
+        // Insert any methods to be run on the activity here
+    }
+
+    public class LocalBinder extends Binder {
+        public WatchdogService getServiceInstance() {
+            return WatchdogService.this;
+        }
     }
 
     /* ********** PUBLIC METHODS FOR BINDING ACTIVITY TO CALL ********* */
+
+    public void registerCallbacks(Callbacks callbacks) {
+        activity = callbacks;
+    }
 
     /**
      * Updates the threshold of the active gps manager
