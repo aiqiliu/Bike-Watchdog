c840ef7882c3cb25d2e519be9d4fe435494baa3c
William <williamstogin2018@u.northwestern.edu>
2017-04-30 12:44:59 -0600

    Added update methods to the service

--- phone/src/main/java/bicyclewatchdog/com/bicyclewatchdog/WatchdogService.java
+++ phone/src/main/java/bicyclewatchdog/com/bicyclewatchdog/WatchdogService.java
@@ -3,6 +3,7 @@ package bicyclewatchdog.com.bicyclewatchdog;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import bicyclewatchdog.com.bicyclewatchdog.gps_management.GpsManager;
 
@@ -25,4 +26,32 @@ public class WatchdogService extends Service {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    /* ********** PUBLIC METHODS FOR BINDING ACTIVITY TO CALL ********* */
+
+    /**
+     * Updates the threshold of the active gps manager
+     * @param threshold in meters
+     */
+    public void updateThreshold(float threshold) {
+        Log.v(TAG, "Updating threshold to " + Float.toString(threshold));
+        // TODO: call mGpsManager.updateThreshold(threshold);
+    }
+
+    /**
+     * Updates the type that the messenger uses to send messages
+     * @param type BICYCLE or PHONE
+     */
+    public void updateType(int type) {
+        Log.v(TAG, "Updating type to " + Integer.toString(type));
+        // TODO: define type enum
+        // TODO: implement this
+    }
+
+    public void updatePhone(String number) {
+        Log.v(TAG, "Updating phone to " + number);
+        // TODO: implement updatePhone
+    }
+
+
 }
