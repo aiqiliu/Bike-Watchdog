844e62f6fba29e5e1811ad58a15661e47ace127e
William <williamstogin2018@u.northwestern.edu>
2017-04-30 12:39:56 -0600

    Added the pair clicked button function

--- phone/src/main/java/bicyclewatchdog/com/bicyclewatchdog/MainActivity.java
+++ phone/src/main/java/bicyclewatchdog/com/bicyclewatchdog/MainActivity.java
@@ -2,6 +2,8 @@ package bicyclewatchdog.com.bicyclewatchdog;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
         // Set the change listener for phone number
         this.findViewById(R.id.editTextPhone)
                 .setOnFocusChangeListener(new PhoneNumberChangedListener());
+    }
+
+    public void onPairClicked(View v) {
+        Log.v(TAG, "Pair clicked.");
+
     }
 
 }
